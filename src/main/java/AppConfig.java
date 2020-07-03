dd48f43b1ce7031c8aa93a2f3f71f249e71f1720
ulkhnv <pudgebek@gmail.com>
2020-07-03 18:02:46 +0600

    Initial commit

---
+++ src/main/java/AppConfig.java
@@ -0,0 +1,23 @@
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
+
+import static org.springframework.beans.factory.config.BeanDefinition.SCOPE_PROTOTYPE;
+
+@Configuration
+public class AppConfig {
+
+    @Bean(name = "helloworld")
+    public HelloWorld getHelloWorld() {
+        HelloWorld helloWorld = new HelloWorld();
+        helloWorld.setMessage("Hello World!");
+        return helloWorld;
+    }
+
+    @Bean(name = "cat")
+    @Scope(SCOPE_PROTOTYPE)
+    public Cat getCat() {
+        Cat cat = new Cat();
+        return cat;
+    }
+}
